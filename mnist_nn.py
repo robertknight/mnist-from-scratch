5e932bf15d57cbcb537e3c075afce42397ddad9c
Robert Knight <robertknight@gmail.com>
2018-09-30 14:02:43 +0100

    Support running `mnist_nn` with either basic or convolutional models
    Add `--model` argument to specify the network model to use, and
    re-introduce the basic non-convolutional model for comparison.
    
    Also update the README with initial performance results for the conv2d
    model.

--- mnist_nn.py
+++ mnist_nn.py
@@ -2,9 +2,9 @@
 MNIST handwritten digit classifier neural net.
 """
 
+import argparse
 import random
 import time
-import sys
 
 import numpy as np
 
@@ -426,26 +426,44 @@ class Model:
         return 1 - (errors / len(data))
 
 
-def train_and_test(dataset_path):
+def train_and_test(dataset_path, model='basic'):
     # Debugging.
     np.seterr(divide='raise')
 
-    # Load train and test datasets.
-    print('reading data...')
-    train_images, train_labels, test_images, test_labels = load_mnist_dataset(dataset_path, (28, 28))
+    if model == 'conv2d':
+        # Load train and test datasets.
+        print('reading data...')
+        train_images, train_labels, test_images, test_labels = load_mnist_dataset(dataset_path, (28, 28))
 
-    model = Model(layers=[
-        Conv2DLayer(32, (3, 3), activation=Relu()),
-        FlattenLayer(),
-        Layer(32, name='relu', activation=Relu()),
-        Layer(10, name='softmax', activation=Softmax()),
-    ], input_size=(28, 28))
+        model = Model(layers=[
+            Conv2DLayer(32, (3, 3), activation=Relu()),
+            FlattenLayer(),
+            Layer(32, name='relu', activation=Relu()),
+            Layer(10, name='softmax', activation=Softmax()),
+        ], input_size=(28, 28))
 
-    print('training model...')
-    model.fit(train_images, train_labels,
-              batch_size=32, epochs=10, learning_rate=0.1,
-              learning_rate_decay=0.1,
-              loss_op=CategoricalCrossentropy())
+        print('training model...')
+        model.fit(train_images, train_labels,
+                  batch_size=32, epochs=10, learning_rate=0.15,
+                  learning_rate_decay=0.1,
+                  loss_op=CategoricalCrossentropy())
+    elif model == 'basic':
+        train_images, train_labels, test_images, test_labels = load_mnist_dataset(dataset_path, (28, 28))
+
+        model = Model(layers=[
+            Conv2DLayer(32, (3, 3), activation=Relu()),
+            FlattenLayer(),
+            Layer(32, name='relu', activation=Relu()),
+            Layer(10, name='softmax', activation=Softmax()),
+        ], input_size=(28, 28))
+
+        print('training model...')
+        model.fit(train_images, train_labels,
+                  batch_size=32, epochs=10, learning_rate=0.15,
+                  learning_rate_decay=0.1,
+                  loss_op=CategoricalCrossentropy())
+    else:
+        raise Exception(f'Unknown model {model}')
 
     print('evaluating model...')
     accuracy = model.evaluate(test_images, test_labels)
@@ -453,5 +471,10 @@ def train_and_test(dataset_path):
 
 
 if __name__ == '__main__':
-    dataset = sys.argv[1]
-    train_and_test(dataset)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m, --model', dest='model', choices=('basic', 'conv2d'),
+                        default='basic', help='Network model to use')
+    parser.add_argument('dataset_path', help='Path to MNIST-formatted dataset')
+    args = parser.parse_args()
+
+    train_and_test(dataset_path=args.dataset_path, model=args.model)
